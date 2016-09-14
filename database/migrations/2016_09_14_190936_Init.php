5835ff600b80a5edef3bb82d017ca63a04808522
Alexey Vishnyakov <android@zapiataia.ru>
2016-09-14 22:25:19 +0300

    Слил миграции в одну

--- database/migrations/2016_09_14_190936_Init.php
+++ database/migrations/2016_09_14_190936_Init.php
@@ -3,12 +3,14 @@
 use App\Http\Controllers\WeKastController;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Storage;
 
-class ModelPresentation extends Migration
+class Init extends Migration
 {
-    const TABLE_NAME = 'presentations';
+    const TABLE_USERS = 'users';
 
+    const TABLE_PRES = 'presentations';
     /**
      * Run the migrations.
      *
@@ -16,19 +18,35 @@ class ModelPresentation extends Migration
      */
     public function up()
     {
+
+        Schema::create(self::TABLE_USERS, function(Blueprint $table)
+        {
+            $table->engine = 'MyISAM';
+            $table->increments('id');
+            $table->string('login')->unique();
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->smallInteger('code')->nullable();
+            $table->string('confirmed', 32)->nullable();
+            $table->index('confirmed');
+            $table->timestamps();
+        });
+
+
         Storage::makeDirectory(WeKastController::PRESENTATIONS_PATH, 0777);
         $dir = 'storage/app/' . WeKastController::PRESENTATIONS_PATH;
         if (file_exists($dir)) {
             chmod($dir, 0777);
         }
 
-        Schema::create(self::TABLE_NAME, function(Blueprint $table)
+        Schema::create(self::TABLE_PRES, function(Blueprint $table)
         {
             $table->engine = 'MyISAM';
             $table->increments('id');
             $table->unsignedInteger('user_id');
             $table->foreign('user_id')->references('id')->on('user');
             $table->string('name');
+            $table->unique('user_id', 'name');
             $table->timestamps();
         });
     }
@@ -40,7 +58,11 @@ class ModelPresentation extends Migration
      */
     public function down()
     {
+
+
         Storage::deleteDirectory(WeKastController::PRESENTATIONS_PATH);
-        Schema::drop(self::TABLE_NAME);
+        Schema::drop(self::TABLE_PRES);
+
+        Schema::drop(self::TABLE_USERS);
     }
 }
